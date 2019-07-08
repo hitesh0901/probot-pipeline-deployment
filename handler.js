d2e0bf2231ed4bf96669e1505cdff471f59b4bda
Hitesh Sharma <hitshar@amazon.com>
2019-07-08 13:18:30 -0700

    handler code and endpoint url

--- handler.js
+++ handler.js
@@ -1,9 +1,13 @@
-'use strict';
-var moment = require('moment');
+// 'use strict';
+// var moment = require('moment');
 
-exports.probot = (event, context, callback) => {
-    callback(null, {
-        "statusCode": 200,
-        "body": "Hello World!!"
-    });
-}
+// exports.probot = (event, context, callback) => {
+//     callback(null, {
+//         "statusCode": 200,
+//         "body": "Hello World!!"
+//     });
+// }
+
+const { serverless } = require('@probot/serverless-lambda')
+const appFn = require('./')
+module.exports.probot = serverless(appFn)
